1e032880c498764d9ac4c13cf628b1ef1743f1a5
Charlie Hale <147997731+CJPro25@users.noreply.github.com>
2025-03-10 14:29:44 +0000

    umm mabey

--- Assets/Scenes/VaseBreak.cs
+++ Assets/Scenes/VaseBreak.cs
@@ -2,14 +2,28 @@ using UnityEngine;
 public class VaseBreak : MonoBehaviour
 {
     public GameObject vase;
+    Vector3 lastvelocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Update()
+    {
+        if(GetComponent<Rigidbody>().linearVelocity != new Vector3(0, 0, 0))
+        {
+            lastvelocity = GetComponent<Rigidbody>().linearVelocity;
+        }
+        Debug.Log(lastvelocity);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Breakable")
         {
-            Instantiate(vase, transform.position, transform.rotation);
+            vase = Instantiate(vase, transform.position, transform.rotation);
+            Debug.Log(vase);
+            foreach (Rigidbody t in vase.transform.GetComponentsInChildren<Rigidbody>())
+            {
+                t.AddForce(lastvelocity * GetComponent<Rigidbody>().mass * 10);
+                Debug.Log(t.linearVelocity);
+            }
             Destroy(gameObject);
-            Debug.Log("smash.mp3");
         }
     }
 }
